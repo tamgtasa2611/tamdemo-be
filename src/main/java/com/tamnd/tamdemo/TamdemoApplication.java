f342d76f8924970a10cb806e4355942470125501
Nguyen Duc Tam <tamgtasa2611@gmail.com>
2024-10-22 20:36:24 +0700

    init

---
+++ src/main/java/com/tamnd/tamdemo/TamdemoApplication.java
@@ -0,0 +1,13 @@
+package com.tamnd.tamdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TamdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tamnd/tamdemo/TamdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamnd.tamdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
